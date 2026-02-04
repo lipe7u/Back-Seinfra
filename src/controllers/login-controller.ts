30c6d37459d0736a70191c56f7d63787fb7a2c81
Felipe silva <lipesx07@gmail.com>
2026-02-04 15:28:47 -0300

    repassando false no secure

--- src/controllers/login-controller.ts
+++ src/controllers/login-controller.ts
@@ -4,13 +4,12 @@ import { LoginB } from "../interface/auth-interfaces";
 
 export const login = async (
   request: FastifyRequest<{ Body: LoginB }>,
-  reply: FastifyReply
-) => {
+  reply: FastifyReply) => {
   try {
     const token = await loginUserService(reply.server, request.body);
     reply.setCookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: false,
       maxAge: 24 * 60 * 60,
       sameSite: "lax",
       path: "/",
